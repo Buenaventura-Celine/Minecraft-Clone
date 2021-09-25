118f0febd9027babeb3b246b9eb0beb52efd97f8
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-09-25 18:59:26 +0800

    Player component added

---
+++ src/Player.js
@@ -0,0 +1,23 @@
+import { useSphere } from "use-cannon";
+
+export const Player = (props) => {
+    const { camera } = useThree()
+
+    const [ref, api] = useSphere(() => ({
+        mass: 1,
+        type: "Dynamic",
+        position: [0, 10, 0],
+        ...props
+    }))
+
+    useFrame(() => {
+        camera.position.copy(ref.current.position)
+    })
+
+    return (
+        <>
+            <PointerLockControls/>
+            <mesh ref={ref}></mesh>
+        </>
+    )
+}

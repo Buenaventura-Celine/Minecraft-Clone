351dbf7f59a0afc469018a31872d15f11570797c
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-09-25 18:28:45 +0800

    Creating the settings of the game

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'; 
+import { Physics } from 'use-cannon';
 
 function App() {
   return (
-    <div>
-      Minecraft
-    </div>
+    <Canvas>
+      <Sky sunPosition={new Vector3(100, 10, 100)}/>
+      <ambientLight intensity={0.3}/>
+      <pointLight castShadow intensity={0.8} position={[100, 100, 100]}/>
+      <Physics>
+        <Ground/>
+        <Player/>
+      </Physics>
+    </Canvas>
   );
 }
 
